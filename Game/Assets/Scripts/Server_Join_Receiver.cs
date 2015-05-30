9c32ec03376de79a1a0b5ca6bfc49b6b78272c55
lycantropus <mechanical.maggot.0@gmail.com>
2015-05-30 20:30:13 +0100

    controller almost done until game phase

---
+++ Game/Assets/Scripts/Server_Join_Receiver.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+public class Server_Join_Receiver : MonoBehaviour {
+
+	// Use this for initialization
+     int listenerPort;
+    IPEndPoint remote_end, client_end;
+    
+    UdpClient udpClient;
+    
+
+	void Start () {
+        StartJoinReceiver();
+	}
+
+    void StartJoinReceiver()
+    {
+        listenerPort = 2224;
+
+        remote_end = new IPEndPoint(IPAddress.Any, listenerPort);
+        udpClient = new UdpClient(remote_end);
+        udpClient.BeginReceive(new AsyncCallback(JoinLookup), null);
+
+    }
+
+    void JoinLookup(IAsyncResult result)
+    {
+        //received join message
+        byte[] receiveBytes = udpClient.EndReceive(result, ref remote_end);
+
+        //sending confirmation message to client
+
+        IPAddress clientAddress = IPAddress.Parse(remote_end.Address.ToString());
+        Debug.Log("received join from " + clientAddress.ToString());
+        client_end = new IPEndPoint(clientAddress, listenerPort);
+
+        Byte[] buffer = Encoding.Unicode.GetBytes("OK");
+        udpClient.Send(buffer, buffer.Length, client_end);
+        Debug.Log("OK sent to client");
+
+       
+        string strData = System.Text.Encoding.Unicode.GetString(receiveBytes);
+        
+        Debug.Log(strData);
+        udpClient.BeginReceive(new AsyncCallback(JoinLookup), null);
+
+    }
+}
---
+++ Game/Assets/Scripts/Server_Mc_Advertiser.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Server_Mc_Advertiser : MonoBehaviour {
+
+
+    UdpClient udpclient;
+    IPEndPoint remoteep;
+    Byte[] buffer;
+
+	// Use this for initialization
+	void Start () {
+         udpclient = new UdpClient();
+
+        IPAddress multicastaddress = IPAddress.Parse("239.0.0.222");
+        udpclient.JoinMulticastGroup(multicastaddress);
+        remoteep = new IPEndPoint(multicastaddress, 2223);
+
+       buffer = null;
+
+        //Console.WriteLine("Press ENTER to start sending messages");
+        //Console.ReadLine();
+
+        /*
+        for (int i = 0; i <= 8000; i++)
+        {
+            buffer = Encoding.Unicode.GetBytes(i.ToString());
+            udpclient.Send(buffer, buffer.Length, remoteep);
+            Debug.Log("Sent " + i);
+        }
+         */
+
+        //Console.WriteLine("All Done! Press ENTER to quit.");
+        //Console.ReadLine();
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("jump");
+            buffer = Encoding.Unicode.GetBytes("oi gatos");
+            udpclient.Send(buffer, buffer.Length, remoteep);
+            Debug.Log("Sent " + "gatos");
+        }
+
+
+       
+
+	
+	}
+}

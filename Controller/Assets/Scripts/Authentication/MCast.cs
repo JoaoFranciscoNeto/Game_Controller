8d1eef8dc04a4844e369d40f49a00dad8006a9c0
lycantropus <mechanical.maggot.0@gmail.com>
2015-05-30 13:45:04 +0100

    MC working
    need to split in 2 projs

---
+++ Controller/Assets/Scripts/Authentication/MCast.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+
+public class MCast : MonoBehaviour {
+
+    public int startupPort = 5100;
+    public string groupAddress = "239.0.0.222";
+
+    UdpClient udpClient;
+    IPEndPoint remote_end;
+
+	// Use this for initialization
+	void Start () {
+
+        StartGameClient();
+	}
+
+    void StartGameClient()
+    {
+        
+        remote_end = new IPEndPoint(IPAddress.Any, startupPort);
+        udpClient = new UdpClient(remote_end);
+        udpClient.JoinMulticastGroup(IPAddress.Parse(groupAddress));
+
+        Debug.Log("Begin Receive");
+
+        udpClient.BeginReceive(new AsyncCallback(ServerLookup), null);
+    }
+
+    void ServerLookup(IAsyncResult result)
+    {
+            byte[] receiveBytes = udpClient.EndReceive(result, ref remote_end);
+            string strData = System.Text.Encoding.Unicode.GetString(receiveBytes);
+            Debug.Log(strData);
+
+        udpClient.BeginReceive(new AsyncCallback(ServerLookup), null);
+   
+    }
+
+	
+}

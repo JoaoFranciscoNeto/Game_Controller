637d707862f3f7432dcb1998860009019efe50c2
Johnstein <joao.neto.feup@gmail.com>
2015-05-30 11:01:13 +0100

    First Commit

---
+++ Controller/Assets/Scripts/MainMenu/PopulateMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopulateMenu : MonoBehaviour {
+
+    public GameObject sampleButton;
+
+    public Transform parentContainer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            addButton();
+        }
+	
+	}
+
+    private void addButton()
+    {
+        GameObject newButton = Instantiate(sampleButton) as GameObject;
+
+        newButton.transform.SetParent(parentContainer);
+    }
+}
